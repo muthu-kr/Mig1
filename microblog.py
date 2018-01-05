7169fd5cdd9d147075a612b7db16e730175c6adc
muthu <coderbro@hotmail.com>
2018-01-06 00:59:57 +0530

    end of Database

--- microblog.py
+++ microblog.py
@@ -1,2 +1,6 @@
 from application1 import app, db
 from application1.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+	return {'db': db, 'User': User, 'Post': Post}

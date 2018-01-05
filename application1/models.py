dcda85ba6ceee020438b151d08a5002bf2604acb
muthu <coderbro@hotmail.com>
2018-01-05 12:20:28 +0530

    Changed Post class - mistake 2

--- application1/models.py
+++ application1/models.py
@@ -17,5 +17,5 @@ class Post(db.Model):
 	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-	def __repr__():
+	def __repr__(self):
 		return '<Post {}>'.format(self.body)
